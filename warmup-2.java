d8f0dc54c15bbde23457077bdc1a588cb5176f00
Jose Dominguez <32556858+jcd1071@users.noreply.github.com>
2018-06-11 17:57:13 -0400

    warmup-2.java

--- warmup-2.java
+++ warmup-2.java
@@ -35,7 +35,7 @@ overlapping is allowed, so "xxx" contains 2 "xx".*/
 
 int countXX(String str) {
     int count = 0;
-    
+   
     for (int i =0;i<str.length()-1;i++){
         if (str.substring(i, i+2).equals("xx")) count ++;
     }
